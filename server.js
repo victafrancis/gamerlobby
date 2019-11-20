1949e444581886c57acdd067db5dac8acd500aa2
Francis Victa <francisvicta45@gmail.com>
2019-11-20 14:19:30 -0500

    edited json

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/angular8-meanstack-angular-material'))
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular8-meanstack-angular-material/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
