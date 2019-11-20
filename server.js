41d6f74efdbcdcc68f96d357c41f6aaeab151e4d
Francis Victa <francisvicta45@gmail.com>
2019-11-20 18:45:18 -0500

    changed project name lab08angular

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular8-meanstack-angular-material'));
+app.use(express.static(__dirname + '/dist/lab08angular'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/angular8-meanstack-angular-material/index.html'));
+res.sendFile(path.join(__dirname+'/dist/lab08angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
