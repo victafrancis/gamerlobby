9c5debf4047160e6b772cf61f62596de5c3383d6
Francis Victa <francisvicta45@gmail.com>
2019-11-20 19:00:22 -0500

    change db name

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-  db: 'mongodb://admin:admin@cluster0-shard-00-00-ailrl.mongodb.net:27017,cluster0-shard-00-01-ailrl.mongodb.net:27017,cluster0-shard-00-02-ailrl.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+  db: 'mongodb+srv://admin:admin@cluster0-ailrl.mongodb.net/test?retryWrites=true&w=majority'
 };
